713c6cd98186d79a998f1ec163645daa8ba99eca
Yousef Farge <yousef.farge@gmail.com>
2025-02-18 19:08:51 -0600

    feat: setup migrations

--- data-access/scripts/init_db.sh
+++ data-access/scripts/init_db.sh
@@ -2,6 +2,14 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v goose)" ]; then
+    echo >&2 "Error: goose is not installed."
+    echo >&2 "Use:"
+    echo >&2 "  go install github.com/pressly/goose/v3/cmd/goose@latest"
+    echo >&2 "to install it."
+    exit 1
+fi
+
 # Check if a custom parameter has been set, otherwise use default values
 DB_PORT="${POSTGRES_PORT:=5432}"
 SUPERUSER="${SUPERUSER:=postgres}"
@@ -32,7 +40,7 @@ then
         "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
         "healthy" \
     ]; do
-       >&2 echo "Postgres is still unavailable - sleelping"
+       >&2 echo "Postgres is still unavailable - sleeping"
        sleep 1
     done
 
@@ -44,8 +52,23 @@ then
     GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB";
     docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
 
+    # Create the application database
+    CREATE_DB_QUERY="CREATE DATABASE ${APP_DB_NAME} OWNER ${APP_USER};"
+    docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_DB_QUERY}"
+
+fi
+
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 
+DATABASE_DRIVER=postgres
 DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
-export DATABASE_URL
+MIGRATION_DIR=./migrations
+
+export GOOSE_DRIVER="$DATABASE_DRIVER"
+export GOOSE_DBSTRING="$DATABASE_URL"
+export GOOSE_MIGRATION_DIR="$MIGRATION_DIR"
+
+goose up
+
+>&2 echo "Postgres has been migrated, ready to go!"
